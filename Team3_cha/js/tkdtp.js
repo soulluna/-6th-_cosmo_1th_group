f5963df354f144b1402d72d48045fec3abef20e5
KurumaHJ <54051930+dktmdh@users.noreply.github.com>
2019-09-10 22:45:30 +0900

    Cha Hj
    1. 메인 게시판 사이드 게시판이동 글씨가 아닌 배경으로도 넘어가게끔 바꿈
    
    2. 글쓰기 게시판에서 돌아가기 클릭하면 경고창후 확인하면 전체게시판으로 넘아가고 취소하면 현상유지로 바꿈

--- Team3_cha/js/tkdtp.js
+++ Team3_cha/js/tkdtp.js
@@ -7,4 +7,13 @@ $(function(){
             // 취소 버튼 클릭 시 동작
             alert("삭제를 취소했습니다.");}
     });
+    
+    $(".back").click(function(){
+        if (confirm("확인시 내용이 있을경우 사라집니다.")) {
+            // 확인 버튼 클릭 시 동작
+            location.replace('./noticeBoardMain.html');
+        } else {
+            // 취소 버튼 클릭 시 동작
+            }
+    });
 });
