04009098f7c643c83622efc83239bdf6d3d00e55
yunkyeongKim <54051490+kan03021@users.noreply.github.com>
2019-09-16 23:15:24 +0900

    정보수정 스크립트 추가
    1. 정보수정 스크립트 추가

---
+++ Team2_kim/member/js/pwd.js
@@ -0,0 +1,43 @@
+function validate(){
+    var regexpPwd = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,15}$/ //비밀번호 정규식
+    var chg_pwd = document.getElementById("chg_pwd");
+
+    //----------------------------------------------------
+
+    if(pwd.value=="") {
+       alert("현재 비밀번호를 입력해주세요");
+       pwd.focus();
+       return false;
+   }
+
+
+    if(!check(regexpPwd,chg_pwd,"비밀번호는 8~15자리의 특수문자 포함 문자열입니다.")){
+        chg_pwd.focus();
+        return false;
+    }
+
+    if(chg_pwd.value=="") {
+       alert("변경할 비밀번호를 입력해주세요");
+       chg_pwd.focus();
+       return false;
+   }
+
+
+    if(chg_pwd.value != chg_pwd_con.value){
+        alert("비밀번호가 다릅니다. 다시 확인해주세요.");
+        chg_pwd_con.value = "";
+        chg_pwd_con.focus();
+        return false;
+    } 
+   alert("비밀번호가 변경되었습니다.");
+}
+
+function check(regexpId, what, message) {
+   if(regexpId.test(what.value)) {
+       return true;
+   }
+   alert(message);
+   what.value = "";
+   what.focus();
+   //return false;
+}
